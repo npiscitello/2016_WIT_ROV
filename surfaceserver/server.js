386b302707b2b0e21666f1c17e57bf7604c1bd06
Nick P <nb.piscitello@gmail.com>
2016-03-28 19:44:59 -0400

    made variables work between Express and Jade, made stylesheets work between Stylo and Jade

--- surfaceserver/server.js
+++ surfaceserver/server.js
@@ -2,17 +2,21 @@ var express = require("express")
 var logger = require("morgan")
 var app = express()
 
+// set path for static files
+app.use(express.static(__dirname + "/static"))
+
 app.get('/', function(req, res) {
   // test for localhost login
   var address = req.socket.remoteAddress
   localhost_addr = /127.0.0.1/
   if(localhost_addr.test(address)) {
-    res.send("Welcome! You're logged in from the local machine!")
+    res.render(__dirname + "/source/templates/localhost.jade")
   } else {
-    res.send("INTRUDER! You're logged in from " + address + "!")
+    res.render(__dirname + "/source/templates/remote.jade", {address: address})
   }
 })
 
+// listen to port 3000 (or another port?)
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on http://localhost:" + (process.env.PORT || 3000))
 })
