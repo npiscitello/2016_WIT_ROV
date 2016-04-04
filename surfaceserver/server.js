7b4fa80a3f4d546a044883eef7c61dfab9c107c1
Nick P <nb.piscitello@gmail.com>
2016-04-04 18:33:50 -0400

    caught lack of rov error so the server can run independently

--- surfaceserver/server.js
+++ surfaceserver/server.js
@@ -1,9 +1,13 @@
-//*** HTTP STUFF ***//
 // imports
 var express = require("express")
 var logger = require("morgan")
 var app = express()
+page_socket = require("express-ws")(app)
+rov_socket = new require("net").Socket()
 
+
+
+//*** HTTP STUFF ***//
 // constants
 const HTTP_PORT = 3000
 const LOCKOUT = false   // set flag to true to lock control to localhost only.
@@ -24,16 +28,34 @@ app.get('/', function(req, res) {
   }
 })
 
+
+
 //*** SOCKET STUFF ***//
-// imports
-page_socket = require("express-ws")(app)
-rov_socket = new require("net").Socket()
+// variable to hold the state of the socket
+open = false
 
 // set options and connect to the rov
 rov_socket.readable = true
 rov_socket.writable = true
 rov_socket_options = {"address":"127.0.0.1", "port":"8100"}
 rov_socket.connect(rov_socket_options) 
+
+// listen for an unsuccessful connection to the rov
+rov_socket.on("error", function() {
+  console.log("connection to the rov failed")
+})
+
+// listen for a closed connection to the rov
+rov_socket.on("close", function() {
+  console.log("connection to the rov closed")
+  open = false
+})
+
+// listen for a successful connection to the rov
+rov_socket.on("connect", function() {
+  console.log("connected to the rov at " + rov_socket.remoteAddress + ":" + rov_socket.remotePort)
+  open = true
+})
 
 // handle page communications
 app.ws('/', function(ws, req) {
@@ -42,7 +64,11 @@ app.ws('/', function(ws, req) {
   // listen for messages from the page
   ws.on('message', function(msg) {
     console.log("received from page: " + msg)
-    rov_socket.write(msg + "\r\n")
+    if(open) {
+      rov_socket.write(msg + "\r\n")
+    } else {
+      ws.send("ROV not connected!")
+    }
   })
 
   // listen for data from the rov
@@ -50,11 +76,6 @@ app.ws('/', function(ws, req) {
     console.log("received from rov: " + data)
     ws.send("" + data)
   })
-})
-
-// listen for a successful connection to the rov
-rov_socket.on("connect", function() {
-  console.log("connected to the rov at " + rov_socket.remoteAddress + ":" + rov_socket.remotePort)
 })
 
 
