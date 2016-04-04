134317ba50ec51e9fff247dd3f441bd9f28b9e81
Nick P <nb.piscitello@gmail.com>
2016-04-04 19:02:52 -0400

    added GUI cues for ROV connection errors

--- surfaceserver/server.js
+++ surfaceserver/server.js
@@ -65,15 +65,11 @@ app.ws('/', function(ws, req) {
   // listen for messages from the page
   ws.on('message', function(msg) {
     console.log("received from page: " + msg)
-<<<<<<< HEAD
     if(open) {
       rov_socket.write(msg + "\r\n")
     } else {
       ws.send("ROV not connected!")
     }
-=======
-    rov_socket.write(msg + "\n")
->>>>>>> 5396b4943c8494ef84e82c09e75b89393f1ff008
   })
 
   // listen for data from the rov
--- surfaceserver/source/app/localhost.js
+++ surfaceserver/source/app/localhost.js
@@ -1,9 +1,12 @@
 raw_url = window.location.href
-regex = /http(s?):///
-var socket = new WebSocket(raw_url.replace(regex, "ws://"))
+regex = /http(s?):/
+ws_url = raw_url.replace(regex, "ws:")
+var socket = new WebSocket(ws_url)
 
 // wait for the window to load before trying to do anything...
 window.onload = function() {
+  console.log("Connecting to a websocket at " + ws_url)
+  
   // respond to a button click
   document.getElementById("json_button").onclick = function() {
     socket.send(document.getElementById("json_input").value)
